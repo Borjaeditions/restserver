9898dfeacf7979980430661aa3a4caf3c3d30bb2
Borjaeditions <borja_and_borjaeditions@live.com>
2022-01-03 03:03:32 -0600

    Cuarto commit, aprender a utilizar objetos request y response

--- controller/usuarios.js
+++ controller/usuarios.js
@@ -1,6 +1,11 @@
+const bcryptjs = require('bcryptjs');
 //Sólo es para que aparezca el tipo de dato
 const {response, query} = require('express');
 const Usuario = require('../models/usuarios');
+const {findOne} = require('mongoose');
+const { validationResult } = require('express-validator');
+
+
 const usuariosGet = (req, res, next)=>{
 
     //query permite conocer los parametros que se estén mandando a través del link, se puede separar cada parámetro o asingar algún valor en especifico si el espacio viene vacio
@@ -21,16 +26,58 @@ const usuariosGet = (req, res, next)=>{
 
 const usuariosPost = async (req, res, next)=>{
 
-    const {nombre, correo, password, rol} = req.body;
-    const usuarios = new Usuario({nombre, correo, password, rol});
+    //los errores vienen del middleware que esta entre la ruta y el controlador
+    errores = validationResult(req);
+    //ValidationResult no regresa nada si no hay algún error, por eso se utiliza isEmpity para conocer que el parametro errores este vacio
+    
+    //No veo si haya alguna manera de obtener algún dato "errores" pero sí con req.body
+    console.log(req.body.correo);
 
-    await usuarios.save();
+    console.log(errores);
+    console.log(errores.errors);
+    console.log(errores.errors.value);
+    if (!errores.isEmpty()){
+
+        //console.log("Correo introducido no es valido: ", errores)
+        return res.status(400).json({
+
+            msg: "El correo no es valido",
+            correo: req.body.correo,
+
+        });
+
+    }
+       
+    const {nombre, correo, password, rol} = req.body;
+    const usuario = new Usuario({nombre, correo, password, rol});
+
+    //verificar si el correo existe
+
+    const ExisteCorreo = await Usuario.findOne({correo});
+    if (ExisteCorreo) {
+     
+        return res.status(400).json({
+
+            msg: "El correo ya esta registrado "
+
+        });
+
+    }   
+    
+
+    //Encriptar contraseña
+
+    const salt = bcryptjs.genSaltSync();
+    usuario.password = bcryptjs.hashSync( password, salt);
+    //guardar en base de datos
+
+    await usuario.save();
 
     res.json({
 
         ok: true,
         msg: 'post a API - controlador',
-        usuarios
+        usuario
     });
     //console.log(JSON.stringify(body).cyan);
 }
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const {Router} = require('express');
+const { check } = require('express-validator');
 const { usuariosGet, usuariosPut, usuariosPost, usuariosDelete } = require('../controller/usuarios');
 
 const router = Router();
@@ -6,7 +7,8 @@ const router = Router();
 router.get('/', usuariosGet);
 
 router.put('/:id', usuariosPut);
-router.post('/', usuariosPost);
+//el check prepara la validación del correo para que el controlador reciba como un argumento en el request, tambiénn tratado como req
+router.post('/', [check('correo', "EL correo no es valido").isEmail(),] ,usuariosPost);
 router.delete('/', usuariosDelete);
 
 module.exports = router;
