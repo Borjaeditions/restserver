3c527c8b945924d0fe2d197249da58adf91d4cfb
Borjaeditions <borja_and_borjaeditions@live.com>
2022-01-08 00:11:18 -0600

    validación de rol -  post a contra base de datos

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const {Router} = require('express');
 const { check } = require('express-validator');
 const { usuariosGet, usuariosPut, usuariosPost, usuariosDelete, usuariosPatch } = require('../controller/usuarios');
 const { validarCampos } = require('../middlewares/validar-campos');
+const Role = require('../models/role');
 
 const router = Router();
 
@@ -15,7 +16,18 @@ router.post('/', [
     check('password', "La contraseña es obligatorio y más de 6 letras").isLength({min: 6}),
     //check('password', "la contraseña debe contener un caracter especial").matches( "1234567890abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ!$#%/()=?¡+" ),
     check('correo', "EL correo no es valido").isEmail(),
-    check('rol', 'No es un rol valido').isIn(['ADMIN_ROLE', 'USER_ROLE'])
+    check('rol', 'No es un rol valido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    //validación personalizada, ".custom" va a recibir como argumento el valor que estoy evaluando del body, si rol no trae nada se le asgina el string vacio
+    check('rol').custom( async(rol = '') =>{
+
+        //Esta función evalua que el rol exista dentro de la base de datos
+        const existeRol = await Role.findOne({rol});
+        if (!existeRol){
+
+            throw new Error(`El rol ${rol} no existe en la base de datos`);
+
+        }
+    }), 
 ],
 validarCampos,
 usuariosPost);
